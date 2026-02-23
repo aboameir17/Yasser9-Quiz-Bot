584c1d6adb0f4f7be85873b081fb0b34458e14ca
yasserQuiz <aboameir17@gmail.com>
2026-02-23 09:35:21 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,9 @@ def get_main_control_kb(user_id):
     return kb
 
 # ==========================================
-    def get_categories_kb(user_id):
+
+# ==========================================
+def get_categories_kb(user_id):
     kb = InlineKeyboardMarkup(row_width=1).add(
         InlineKeyboardButton("➕ إضافة قسم جديد", callback_data=f"add_new_cat_{user_id}"),
         InlineKeyboardButton("📋 قائمة الأقسام", callback_data=f"list_{user_id}"),
